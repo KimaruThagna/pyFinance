5026c57b2a4b01339d07356cd0e58e87e44cbaee
Kimaru thagana <macbuntu@localhost.localdomain>
2020-07-01 20:55:43 +0300

    dcf formula

---
+++ dcf.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import os
+
+'''
+The discounted Cashflow models is a method of discounting future cashflows of a business at a considerable rate 
+to bring it to today's value. With this formula, one can determine a reasonable intrinsic value of the business they wish to buy
+Owners earnings=Cashflow for owners=Operating cashflow-maintenance capEx. If you assume maintenance capEx=total capEx, 
+then Owners earnings=FreeCashFlow(FCF)
+'''
+
+def dcf(owners_earnings, growth_rate_5, growth_rate_10,
+        risk_free_discount_rate, required_return_rate,
+        terminal_val_growth):
+    pass
+
