4e663505d82826d99c592c83012afbece92cec47
Kimaru thagana <macbuntu@localhost.localdomain>
2020-07-03 15:25:40 +0300

    determine projected cashflows in 10 yrs and terminal value

--- dcf.py
+++ dcf.py
@@ -14,5 +14,26 @@ def dcf(free_cashflow, maintenance_capex_percentage, capex, growth_rate_5, growt
         terminal_val_growth=0):
 
     owners_earnings = free_cashflow - (capex*maintenance_capex_percentage)
-    discounted_cashflows = []
-    # perform cashflow projections for the first 5 years and discount to present value
+    projected_cashflows = []
+    year_10_val = 0
+    # perform cashflow projections for the first 5 years
+    for year in range(1,6):
+        current_cashflow = owners_earnings
+        data_line = [f'year{year}', current_cashflow]
+        current_cashflow += growth_rate_5*current_cashflow # same as current_cashflow = 1+growth_rate_5*current_cashflow
+        projected_cashflows.append(data_line)
+    # perform cashflow projections for the next 5 years
+    for year in range(6,11):
+        current_cashflow = owners_earnings
+        data_line = [f'year{year}', current_cashflow]
+        current_cashflow += growth_rate_5 * current_cashflow  # same as current_cashflow = 1+growth_rate_5*current_cashflow
+        projected_cashflows.append(data_line)
+        if year == 10: # calculate terminal value
+            # Formula TV  =  (Owners earnings_Yr10 x (1 + g))  /  (discount_rate – g) g=terminal_growth_rate
+            intrinsic_terminal_value = (year_10_val+(1+terminal_val_growth)) / (risk_free_discount_rate-terminal_val_growth)
+            buy_terminal_value = (year_10_val + (1 + terminal_val_growth)) / (required_return_rate - terminal_val_growth)
+            data_line1 = ["Intrinsic Terminal Value", intrinsic_terminal_value]
+            data_line2 = ["Buy Price Terminal Value", buy_terminal_value]
+            projected_cashflows.append(data_line1)
+            projected_cashflows.append(data_line2)
+
