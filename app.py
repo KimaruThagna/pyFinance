347228dc5efa1f95cfc71c9d6a264bc4f02b2202
Kimaru thagana <macbuntu@localhost.localdomain>
2020-07-04 17:03:22 +0300

    add definitions

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ to bring it to today's value. \n
 With this formula, one can determine a reasonable intrinsic value of the business they wish to buy.\n
 Owners earnings=Cashflow for owners=Operating cashflow-maintenance capEx. If you assume maintenance capEx=total capEx, 
 then Owners earnings=FreeCashFlow(FCF)
-
+Operating Cashflow- cash generated from operating activities. Can be found in the cash statement \n
+Capital Expenditure (CapEx)- Amount spent on capital assets that will generate revenue for the business long term. Some is used in growth, some in maintenance.\n
+Maintenance CapEx percentage- The percentage of capEx that a business used in maintenance of plant property and equipment. \n
+0-5 Year growth Rate- Projected percentage growth that this business will have over the next 5 years on its cashflows\n
+6-10 Year growth Rate- Projected percentage growth that this business will have over the next 6-10 years on its cashflows. Should ideally be lower that the 0-5.\n
+Risk 
 '''
 if page == "DCF":
 
