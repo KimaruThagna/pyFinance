4c63b7a4f0be495a8fae20c7c5dd902cecd7c00b
Kimaru thagana <macbuntu@localhost.localdomain>
2020-07-04 16:57:05 +0300

    introductory text

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ import streamlit as st
 
 st.sidebar.markdown("Welcome to Picture-lytics.")
 page = st.sidebar.selectbox("Choose task", ["DCF", "NPV Cashflow"])# pages
+intro_text = '''
+The discounted Cashflow models is a method of discounting future cashflows of a business at a considerable rate 
+to bring it to today's value. \n 
+With this formula, one can determine a reasonable intrinsic value of the business they wish to buy.\n
+Owners earnings=Cashflow for owners=Operating cashflow-maintenance capEx. If you assume maintenance capEx=total capEx, 
+then Owners earnings=FreeCashFlow(FCF)
 
+'''
 if page == "DCF":
 
-    st.title("Image Classification with Google's Teachable Machine")
-    st.header("Plant Leaf Disease Classification Example")
-    st.write("Upload plant leaf images for quick analysis")
+    st.title("Investment Analysis using DCF Model")
+    st.header("DCF")
+    st.write(intro_text)
     # file upload and handling logic
 
 
