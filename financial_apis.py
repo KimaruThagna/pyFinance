df3d22f6febb57acd880324f6cdeabfdca4783af
Kimaru thagana <macbuntu@localhost.localdomain>
2020-07-05 13:20:33 +0300

    function descriptions

--- financial_apis.py
+++ financial_apis.py
@@ -2,14 +2,21 @@ import requests
 
 root_url = 'https://financialmodelingprep.com/api/v3/'
 
+
+def company_profile(ticker_symbol):
+    pass
+
 def company_financial_statements(ticker_symbol):
     pass
 
-def company_financial_ratios(ticker_symbol):
+def company_financial_ratios_and_metrics(ticker_symbol):
     pass
 
 def company_dcf_analysis(ticker_symbol):
     pass
 
-def company_dcf_api(ticker_symbols):
+def company_dcf_api(ticker_symbol):
+    pass
+
+def company_growth_figures(ticker_symbol):
     pass
