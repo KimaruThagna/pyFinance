13be09846b665eeb6729302f2a5c0ff2f00aa2b1
Kimaru thagana <macbuntu@192.168.1.5>
2020-07-08 18:20:06 +0300

    define data pull function

--- financial_apis.py
+++ financial_apis.py
@@ -5,10 +5,15 @@ load_dotenv()
 root_url = 'https://financialmodelingprep.com/api/v3/'
 
 
-def data_pull(ticker_symbol):
+def data_pull(domain, ticker_symbol):
     key = os.getenv('API_KEY')
     url = f'{root_url}'
-    pass
+    PARAMS = {'apikey': key}
+    try:
+        data = requests.get(url=f'{url}{domain}/{ticker_symbol}', params=PARAMS)
+        return data.json()
+    except Exception as e:
+        return f'An error occured{e}'
 
 def company_profile(ticker_symbol):
     pass
@@ -27,3 +32,4 @@ def company_dcf_api(ticker_symbol):
 
 def company_growth_figures(ticker_symbol):
     pass
+
